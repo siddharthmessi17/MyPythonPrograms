a60c4d8346bcef5be6d8e025e5ff88e5d49efeaf
Siddharth Choudhary <43368247+siddharthmessi17@users.noreply.github.com>
2021-02-21 13:46:18 +0530

    Update upfollowlow.py

--- upfollowlow.py
+++ upfollowlow.py
@@ -1,3 +1,8 @@
+#Problem Statement - Given a string of sixe N. In one move you can change the 
+#case of the character(i.e form lower case to upper case and vice versa).
+#You need to find the minimum number of moves such that zero or more upper 
+#case characters are followed by zero or more lower case characters.
+#constraints: 1 <= N <=10^5, 1 <= len(str) <=10^5.
 def solve(N, str):
     import re
     pat = '^[A-Z+]+[a-z]+$'
